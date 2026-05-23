75cec6a68d6a9a7f77df2483e9a43c3e07bad415
Daniel Dinev <azsumbg@gmail.com>
2026-05-23 20:14:41 +0300

    Add project files.

---
+++ ringhelp/ringhelp.cpp
@@ -0,0 +1,351 @@
+#include "pch.h"
+#include "ringhelp.h"
+
+// EXCEPTION class **************************
+
+dll::EXCEPTION::EXCEPTION(int what) :_what{ what } {};
+
+const wchar_t* dll::EXCEPTION::eWhat()const
+{
+	switch (_what)
+	{
+	case EX_INDEX:
+		return L"Bad index passed to a class method !";
+
+	case EX_PARAM:
+		return L"Bad parameter passed to a class method !";
+
+	case EX_PTR:
+		return L"Bad pointer passed to a class method !";
+
+	case EX_INIT:
+		return L"Error creating BAG container !";
+	}
+
+	return L"Unknown error occurred in a class method !";
+}
+
+/////////////////////////////////////////////
+
+// RANDIT class *****************************
+
+dll::RANDIT::RANDIT()
+{
+	std::random_device rd{};
+	std::seed_seq sq{ rd(),rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+
+	twister = new std::mt19937(sq);
+}
+
+dll::RANDIT::~RANDIT()
+{
+	if (twister)delete twister;
+}
+
+int dll::RANDIT::operator()(int min, int max)
+{
+	if (max < min)return 0;
+
+	std::uniform_int_distribution distrib(min, max);
+
+	return distrib(*twister);
+}
+float dll::RANDIT::operator()(float min, float max)
+{
+	if (max < min)return 0.0f;
+
+	std::uniform_real_distribution<float> distrib(min, max);
+
+	return distrib(*twister);
+}
+
+////////////////////////////////////////////
+
+// PROTON class ***************************
+
+dll::PROTON::PROTON()
+{
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+}
+dll::PROTON::PROTON(float _sx, float _sy)
+{
+	start.x = _sx;
+	start.y = _sy;
+
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+}
+dll::PROTON::PROTON(float _sx, float _sy, float _first_width, float _first_height)
+{
+	start.x = _sx;
+	start.y = _sy;
+
+	_width = _first_width;
+	_height = _first_height;
+
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+}
+
+void dll::PROTON::set_edges()
+{
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+}
+void dll::PROTON::new_dims(float _new_width, float _new_height)
+{
+	_width = _new_width;
+	_height = _new_height;
+
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+}
+
+void dll::PROTON::new_width(float _new_width)
+{
+	_width = _new_width;
+	
+	x_rad = _width / 2.0f;
+	
+	end.x = start.x + _width;
+	
+	center.x = start.x + x_rad;
+}
+void dll::PROTON::new_height(float _new_height)
+{
+	_height = _new_height;
+
+	y_rad = _height / 2.0f;
+
+	end.y = start.y + _height;
+
+	center.y = start.y + y_rad;
+}
+
+void dll::PROTON::set_path(float _ex, float _ey)
+{
+	hor_dir = false;
+	ver_dir = false;
+
+	move_sx = start.x;
+	move_sy = start.y;
+
+	move_ex = _ex;
+	move_ey = _ey;
+
+	if (move_ex == move_sx || (move_ex > move_sx && move_ex <= end.x))
+	{
+		ver_dir = true;
+		return;
+	}
+	if (move_ey == move_sy || (move_ey > move_sy && move_ey <= end.y))
+	{
+		hor_dir = true;
+		return;
+	}
+
+	slope = (move_ey - move_sy) / (move_ex - move_sx);
+	intercept = start.y - start.x * slope;
+}
+
+float dll::PROTON::get_width()const
+{
+	return _width;
+}
+float dll::PROTON::get_height()const
+{
+	return _height;
+}
+
+float dll::PROTON::get_target_x()const
+{
+	return move_ex;
+}
+float dll::PROTON::get_target_y()const
+{
+	return move_ey;
+}
+
+////////////////////////////////////////////
+
+// FIELD class *****************************
+
+dll::FIELD::FIELD(fields _what, float s_x, float s_y) :PROTON(s_x, s_y)
+{
+	type = _what;
+
+	switch (type)
+	{
+	case fields::flat_ground:
+		new_dims(100.0f, 100.0f);
+		break;
+
+	case fields::left_slope:
+		new_dims(100.0f, 100.0f);
+		break;
+
+	case fields::right_slope:
+		new_dims(100.0f, 100.0f);
+		break;
+
+	case fields::background:
+		new_dims(1000.0f, 750.0f);
+		max_frames = 23;
+		break;
+
+	case fields::intro:
+		new_dims(1000.0f, 800.0f);
+		max_frames = 92;
+		break;
+
+	case fields::portal:
+		new_dims(100.0f, 100.0f);
+		max_frames = 23;
+		break;
+	}
+
+	if (type == fields::portal)frame_delay = 3;
+	else frame_delay = 1;
+}
+
+bool dll::FIELD::move(float gear, dirs to_where)
+{
+	float my_speed = _speed + gear / 5.0f;
+
+	switch (to_where)
+	{
+	case dirs::left:
+		start.x -= my_speed;
+		set_edges();
+		if (end.x >= -scr_width)return true;
+
+	case dirs::right:
+		start.x += my_speed;
+		set_edges();
+		if (start.x <= 2.0f * scr_width)return true;
+	}
+
+	return false;
+}
+
+int dll::FIELD::get_frame()
+{
+	--frame_delay;
+	if (frame_delay <= 0)
+	{
+		if (type == fields::portal)frame_delay = 3;
+		else frame_delay = 1;
+		++frame;
+		if (frame > max_frames)frame = 0;
+	}
+	
+	return frame;
+}
+
+void dll::FIELD::Release()
+{
+	delete this;
+}
+
+dll::FIELD* dll::FIELD::create(fields what, float sx, float sy)
+{
+	FIELD* ret = new FIELD(what, sx, sy);
+	
+	return ret;
+}
+
+///////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNCTIONS **********************************************
+
+float dll::distance(D2D1_POINT_2F first, D2D1_POINT_2F second)
+{
+	float a = (float)(pow(abs(second.x - first.x), 2));
+	float b = (float)(pow(abs(second.y - first.y), 2));
+
+	return (float)(sqrt(a + b));
+}
+
+bool dll::intersect(D2D1_RECT_F first, D2D1_RECT_F second)
+{
+	if (!(first.left >= second.right || first.right <= second.left ||
+		first.top >= second.bottom || first.bottom <= second.top))return true;
+
+	return false;
+}
+bool dll::intersect(D2D1_POINT_2F first_center, D2D1_POINT_2F second_center, float first_xrad, float second_xrad,
+	float first_yrad, float second_yrad)
+{
+	if (abs(second_center.x - first_center.x) <= first_xrad + second_xrad
+		&& abs(second_center.y - first_center.y) <= first_yrad + second_yrad)return true;
+
+	return false;
+}
+
+void dll::sort(BAG<D2D1_POINT_2F>& bag, D2D1_POINT_2F ref)
+{
+	if (bag.size() < 2)return;
+	else
+	{
+		bool ok = false;
+		
+		while (!ok)
+		{
+			ok = true;
+
+			for (size_t count = 0; count < bag.size() - 1; ++count)
+			{
+				if (distance(bag[count], ref) > distance(bag[count + 1], ref))
+				{
+					ok = false;
+
+					D2D1_POINT_2F temp = bag[count];
+					bag[count] = bag[count + 1];
+					bag[count + 1] = temp;
+				}
+			}
+		}
+	}
+}
---
+++ ringhelp/ringhelp.h
@@ -0,0 +1,514 @@
+#pragma once
+
+#ifdef RINGHELP_EXPORTS
+#define RINGHELP_API __declspec(dllexport)
+#else
+#define RINGHELP_API __declspec(dllimport)
+#endif
+
+#include <d2d1.h>
+#include <random>
+
+#pragma comment(lib, "d2d1.lib")
+
+constexpr float scr_width{ 1000.0f };
+constexpr float scr_height{ 800.0f };
+
+constexpr float sky{ 50.0f };
+constexpr float ground{ 100.0f };
+
+constexpr int EX_PTR{ 6001 };
+constexpr int EX_INDEX{ 6002 };
+constexpr int EX_PARAM{ 6003 };
+constexpr int EX_INIT{ 6004 };
+constexpr int EX_UNK{ 6005 };
+
+enum class dirs { left = 0, right = 1 };
+enum class actions { run = 0, climb_up = 1, climb_down = 2, stop = 3 };
+enum class creatures { zombie_girl = 0, zombie_boy = 1, zombie_flyer = 2, hero = 3 };
+enum class fields { flat_ground = 0, left_slope = 1, right_slope = 2, background = 3, intro = 4, portal = 5 };
+
+namespace dll
+{
+	class RINGHELP_API EXCEPTION
+	{
+	private:
+		int _what = 0;
+
+	public:
+
+		EXCEPTION(int what);
+
+		const wchar_t* eWhat()const;
+	};
+
+	class RINGHELP_API RANDIT
+	{
+	private:
+		std::mt19937* twister{ nullptr };
+
+	public:
+		RANDIT();
+
+		~RANDIT();
+
+		int operator()(int min, int max);
+		float operator()(float min, float max);
+	};
+
+	template<typename T> class BAG
+	{
+	private:
+		size_t max_size{ 1 };
+		T* basePtr{ nullptr };
+		size_t next_pos{ 0 };
+
+	public:
+		BAG()
+		{
+			basePtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+			if (basePtr == nullptr)throw EXCEPTION(EX_INIT);
+		};
+		BAG(size_t lenght)
+		{
+			max_size = lenght;
+			basePtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+
+			if (basePtr == nullptr)throw EXCEPTION(EX_INIT);
+		}
+		BAG(const BAG& other)
+		{
+			if (other.basePtr)
+			{
+				free(basePtr);
+				basePtr = nullptr;
+
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+
+				basePtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+				
+				if (basePtr == nullptr)throw EXCEPTION(EX_INIT);
+				else if (next_pos > 0)
+						for (size_t count = 0; count < next_pos; ++count)basePtr[count] = other.basePtr[count];
+			}
+			else
+			{
+				free(basePtr);
+				max_size = 1;
+				next_pos = 0;
+
+				basePtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+
+				if (basePtr == nullptr)throw EXCEPTION(EX_INIT);
+			}
+		}
+		BAG(BAG&& other)
+		{
+			free(basePtr);
+			basePtr = nullptr;
+
+			if (other.basePtr)
+			{
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+				basePtr = other.basePtr;
+
+				if (next_pos > 0)
+					for (size_t count = 0; count < next_pos; ++count)basePtr[count] = other.basePtr[count];
+			}
+			else
+			{
+				free(basePtr);
+				max_size = 1;
+				next_pos = 0;
+
+				basePtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+
+				if (basePtr == nullptr)throw EXCEPTION(EX_INIT);
+			}
+
+			other.basePtr = nullptr;
+		}
+
+		~BAG()
+		{
+			free(basePtr);
+		}
+
+		BAG& operator=(const BAG& other)
+		{
+			if (other.basePtr)
+			{
+				free(basePtr);
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+
+				base_ptr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+				if (basePtr == nullptr)throw EXCEPTION(EX_INIT);
+				else if (other.next_pos > 0)
+					for (size_t count = 0; count < next_pos; ++count)basePtr[count] = other.basePtr[count];
+			}
+			else
+			{
+				free(basePtr);
+				max_size = 1;
+				next_pos = 0;
+
+				base_ptr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+				if (basePtr == nullptr)throw EXCEPTION(EX_INIT);
+			}
+
+			return *this;
+		}
+		BAG& operator=(BAG&& other)
+		{
+			if (other.basePtr != nullptr)
+			{
+				free(basePtr);
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+
+				base_ptr = other.basePtr;
+				other.basePtr = nullptr;
+			}
+
+			return *this;
+		}
+
+		T& operator[](size_t index)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (index <= 0)return *basePtr;
+				else if (index >= next_pos)return basePtr[next_pos - 1];
+				else
+			}
+
+			return basePtr[index];
+		}
+
+		void clear()
+		{
+			free(basePtr);
+			max_size = 1;
+			next_pos = 0;
+
+			basePtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+
+			if (!basePtr)throw EXCEPTION(EX_INIT);
+		}
+		bool empty() const
+		{
+			return(next_pos == 0);
+		}
+
+		size_t size() const
+		{
+			return next_pos;
+		}
+		size_t capacity()const
+		{
+			return max_size;
+		}
+
+		void push_front(T element)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (next_pos == 0)
+				{
+					*m_ptr = element;
+					++next_pos;
+					return;
+				}
+				else
+				{
+					if (next_pos + 1 <= max_size)
+					{
+						for (size_t cur_pos = next_pos; cur_pos > 0; --cur_pos)basePtr[cur_pos] = basePtr[cur_pos - 1];
+						*base_ptr = element;
+						++next_pos;
+						return;
+					}
+					else
+					{
+						++max_size;
+						T* tempPtr = reinterpret_cast<T*>(realloc(basePtr, max_size * sizeof(T)));
+
+						if (!tempPtr)throw EXCEPTION(EX_INIT);
+						else
+						{
+							basePtr = tempPtr;
+							for (size_t cur_pos = next_pos; cur_pos > 0; --cur_pos)basePtr[cur_pos] = basePtr[cur_pos - 1];
+							*base_ptr = element;
+							++next_pos;
+							return;
+						}
+					}
+				}
+			}
+		}
+		void push_front(T* element)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (next_pos == 0)
+				{
+					*m_ptr = *element;
+					++next_pos;
+					return;
+				}
+				else
+				{
+					if (next_pos + 1 <= max_size)
+					{
+						for (size_t cur_pos = next_pos; cur_pos > 0; --cur_pos)basePtr[cur_pos] = basePtr[cur_pos - 1];
+						*base_ptr = *element;
+						++next_pos;
+						return;
+					}
+					else
+					{
+						++max_size;
+						T* tempPtr = reinterpret_cast<T*>(realloc(basePtr, max_size * sizeof(T)));
+
+						if (!tempPtr)throw EXCEPTION(EX_INIT);
+						else
+						{
+							basePtr = tempPtr;
+							for (size_t cur_pos = next_pos; cur_pos > 0; --cur_pos)basePtr[cur_pos] = basePtr[cur_pos - 1];
+							*base_ptr = *element;
+							++next_pos;
+							return;
+						}
+					}
+				}
+			}
+		}
+
+		void push_back(T element)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					base_ptr[next_pos] = element;
+					++next_pos;
+					return;
+				}
+				else
+				{
+					++max_size;
+
+					T* tempPtr = reinterpret_cast<T*>(realloc(basePtr, max_size * sizeof(T)));
+
+					if (!tempPtr)throw EXCEPTION(EX_INIT);
+					else
+					{
+						basePtr = tempPtr;
+						basePtr[next_pos] = element;
+						++next_pos;
+						return;
+					}
+				}
+			}
+		}
+		void push_back(T* element)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					base_ptr[next_pos] = *element;
+					++next_pos;
+					return;
+				}
+				else
+				{
+					++max_size;
+
+					T* tempPtr = reinterpret_cast<T*>(realloc(basePtr, max_size * sizeof(T)));
+
+					if (!tempPtr)throw EXCEPTION(EX_INIT);
+					else
+					{
+						basePtr = tempPtr;
+						basePtr[next_pos] = *element;
+						++next_pos;
+						return;
+					}
+				}
+			}
+		}
+
+		void insert(size_t index, T element)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (index < 0 || index > next_pos)return;
+				else
+				{
+					if (next_pos + 1 <= max_size)
+					{
+						for (size_t count = next_pos; count > index; --count)basePtr[count] = basePtr[count - 1];
+						basePtr[index] = element;
+						++next_pos;
+						return;
+					}
+					else
+					{
+						++max_size;
+
+						T* tempPtr = reinterpret_cast<T*>(realloc(basePtr, max_size * sizeof(T)));
+						if (!tempPtr)throw EXCEPTION(EX_INIT);
+						else
+						{
+							base_ptr = tempPtr;
+							for (size_t count = next_pos; count > index; --count)basePtr[count] = basePtr[count - 1];
+							basePtr[index] = element;
+							++next_pos;
+							return;
+						}
+					}
+				}
+			}
+		}
+		void insert(size_t index, T* element)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (index < 0 || index > next_pos)return;
+				else
+				{
+					if (next_pos + 1 <= max_size)
+					{
+						for (size_t count = next_pos; count > index; --count)basePtr[count] = basePtr[count - 1];
+						basePtr[index] = *element;
+						++next_pos;
+						return;
+					}
+					else
+					{
+						++max_size;
+
+						T* tempPtr = reinterpret_cast<T*>(realloc(basePtr, max_size * sizeof(T)));
+						if (!tempPtr)throw EXCEPTION(EX_INIT);
+						else
+						{
+							base_ptr = tempPtr;
+							for (size_t count = next_pos; count > index; --count)basePtr[count] = basePtr[count - 1];
+							basePtr[index] = *element;
+							++next_pos;
+							return;
+						}
+					}
+				}
+			}
+		}
+
+		void erase(size_t index)
+		{
+			if (!basePtr)throw EXCEPTION(EX_PTR);
+			else
+			{
+				if (index < 0 || index >= next_pos)return;
+				else
+				{
+					for (size_t count = index; count < next_pos - 1; ++count)basePtr[count] = basePtr[count + 1];
+					--next_pos;
+				}
+			}
+		}
+	};
+
+	class RINGHELP_API PROTON
+	{
+	protected:
+		float _width{ 1.0f };
+		float _height{ 1.0f };
+
+		bool hor_dir{ false };
+		bool ver_dir{ false };
+		float move_sx{ 0 };
+		float move_sy{ 0 };
+		float move_ex{ 0 };
+		float move_ey{ 0 };
+		float slope{ 0 };
+		float intercept{ 0 };
+
+		float _speed{ 1.0f };
+
+	public:
+		D2D1_POINT_2F start{};
+		D2D1_POINT_2F end{};
+		D2D1_POINT_2F center{};
+
+		float x_rad{ 0 };
+		float y_rad{ 0 };
+
+		PROTON();
+		PROTON(float _sx, float _sy);
+		PROTON(float _sx, float _sy, float _first_width, float _first_height);
+
+		virtual ~PROTON() {};
+
+		void set_edges();
+		void new_dims(float _new_width, float _new_height);
+
+		void new_width(float _new_width);
+		void new_height(float _new_height);
+
+		void set_path(float _ex, float _ey);
+
+		float get_width()const;
+		float get_height()const;
+
+		float get_target_x()const;
+		float get_target_y()const;
+	};
+
+	class RINGHELP_API FIELD :public PROTON
+	{
+	private:
+		int frame = 0;
+		int max_frames = 0;
+		int frame_delay = 1;
+
+		FIELD(fields _what, float s_x, float s_y);
+
+	public:
+		fields type;
+		
+		bool move(float gear, dirs to_where);
+
+		int get_frame();
+
+		void Release();
+
+		static FIELD* create(fields what, float sx, float sy);
+	};
+
+
+
+
+
+
+
+	// FUNCTIONS **********************************************
+
+	RINGHELP_API float distance(D2D1_POINT_2F first, D2D1_POINT_2F second);
+
+	RINGHELP_API bool intersect(D2D1_RECT_F first, D2D1_RECT_F second);
+	RINGHELP_API bool intersect(D2D1_POINT_2F first_center, D2D1_POINT_2F second_center, float first_xrad, float second_xrad,
+		float first_yrad, float second_yrad);
+
+	RINGHELP_API void sort(BAG<D2D1_POINT_2F>& bag, D2D1_POINT_2F ref);
+}
